ddd04050eb716307a63a26f770f9bb3c8f596acf
marcypm <marcel.morin@mail.mcgill.ca>
2018-03-21 20:23:36 -0400

    adder, PC and Definition files created
    Signed-off-by: marcypm <marcel.morin@mail.mcgill.ca>

--- Assignment 4/IF_ID_Stage.vhd
+++ Assignment 4/IF_ID_Stage.vhd
@@ -5,8 +5,8 @@ use work.definitions.all;
 
 entity IF_ID_Stage is
     Port 
-        ( PC_in   : in    addr_type
-        ; PC_out  : out   addr_type
+        ( PC_in   : in    address_type
+        ; PC_out  : out   address_type
         ; inst_in      : in    word_type
         ; inst_out     : out   word_type
         ; insert_stall        : in    std_logic
